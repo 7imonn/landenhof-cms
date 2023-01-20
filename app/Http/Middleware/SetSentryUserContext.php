2d3770c9a8553214c95a2d18e1e863b63a58425d
Kyle Werner <kyle.werner@novu.ch>
2023-01-20 15:07:17 +0100

    add sentry

---
+++ app/Http/Middleware/SetSentryUserContext.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Sentry\State\Scope;
+
+use function Sentry\configureScope;
+
+class SetSentryUserContext
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        foreach (config('auth.guards') as $guardName => $config) {
+            if (! $user = auth($guardName)->user()) {
+                continue;
+            }
+
+            if ($user && app()->bound('sentry')) {
+                configureScope(function (Scope $scope) use ($user): void {
+                    $scope->setUser([
+                        'id' => $user->id,
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ]);
+                });
+            }
+
+            break;
+        }
+
+        return $next($request);
+    }
+}
